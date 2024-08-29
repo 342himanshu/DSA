75145abb4bf553ed14235801cd820ee46795a5cd
342himanshu <342himanshu@gmail.com>
2024-08-29 12:09:26 +0530

    BFS

--- graph/src/main/java/com/graph/Graph.java
+++ graph/src/main/java/com/graph/Graph.java
@@ -1,6 +1,8 @@
 package com.graph;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class Graph {
 
@@ -33,5 +35,23 @@ class Graph {
             }
             System.out.println();
         }
+    }
+
+    public void BFS(int source) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[graph.size()];
+        q.add(source);
+        visited[source] = true;
+        while(!q.isEmpty()) {
+            int n = q.remove();
+            System.out.print(n+" ");
+            for(Integer nbr : graph.get(n)) {
+                if(!visited[nbr]){
+                    q.add(nbr);
+                    visited[nbr] = true;
+                }
+            }
+        }
+        System.out.println();
     }
 }
--- graph/src/main/java/com/graph/GraphApplication.java
+++ graph/src/main/java/com/graph/GraphApplication.java
@@ -4,6 +4,7 @@ public class GraphApplication {
 
 	public static void main(String[] args) {
 		 Graph graph = new Graph(7, false);
+		 graph.addEdge(0, 1);
 		 graph.addEdge(1, 2);
 		 graph.addEdge(2, 3);
 		 graph.addEdge(3, 5);
@@ -13,6 +14,7 @@ public class GraphApplication {
 		 graph.addEdge(3, 4);
 
 		 graph.print();
+		 graph.BFS(1);
 	}
 
 }
